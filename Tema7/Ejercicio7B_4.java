0514eeda2330831f10d4254f99cc8e7fa655bc01
Samuel Valverde Garcia <samuel.ies.valverde@gmail.com>
2018-11-21 14:13:24 +0100

    actualizacion

---
+++ Tema7/Ejercicio7B_4.java
@@ -0,0 +1,48 @@
+public class Ejercicio7B_4{
+  public static void main(String[] arg)
+  throws InterruptedException{
+  System.out.println("Introduce 20 numeros enteros");
+  
+  int[][] num = new int[4][5];   //4 filas 5 columnas
+  int[][] aux = new int [5][6];
+
+  
+  for (int i= 0;i<4;i++){
+    for (int x = 0;x<5;x++){
+      num[i][x] = (int)(((Math.random()*900)+100));
+      aux[i][x] = num[i][x];
+      }
+    }
+  //relleno el arraid num y aux
+  
+  for (int i=0;i<4;i++){
+    for (int x=0;x<5;x++){
+        aux[i][5] += num[i][x];
+      }
+    }
+  //se calcula la suma de las filas en la columna 6
+  
+  
+  for (int i=0;i<6;i++){
+    for (int x=0;x<4;x++){
+        aux[4][i] += aux[x][i];
+        
+      }
+    }
+  //se calcula la suma de las columnas en la fila 5
+  
+  
+  
+  
+  for (int i=0;i<5;i++){
+    System.out.println();
+    for (int x=0;x<6;x++){
+       System.out.print(aux[i][x]+" ");
+       Thread.sleep(1000);
+      }
+    }
+  //se muestra
+  
+    
+  }
+}
